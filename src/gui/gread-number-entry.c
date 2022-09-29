78c8adefdbc845e7dd28e9585df01c1a81e5f83e
xalares <alexis_de-bacco@hotmail.fr>
2022-09-29 13:02:47 +0200

    implementing gread-number-entry

--- src/gui/gread-number-entry.c
+++ src/gui/gread-number-entry.c
@@ -4,7 +4,20 @@
 #include "gread-number-entry.h"
 
 struct _GreadNumberEntry {
-  GtkEditable parent;
-}
+  GtkWidget parent;
+  GtkEntry *entry
+  guint value;
+};
 
 G_DEFINE_TYPE (GreadNumberEntry, gread_number_entry, ADW_TYPE_BIN)
+
+static void
+gread_number_entry_class_init(GreadNumberEntry *klass){
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS(klass);
+  GObjectClass *object_class = G_OBJECT_CLASS(klass);
+}
+
+static void
+gread_number_entry_init(GreadNumberEntry *self){
+
+}
