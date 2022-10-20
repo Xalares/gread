0af6f7181cb6ac9cd00b5e2287108aa3e3826b43
xalares <alexis_de-bacco@hotmail.fr>
2022-10-20 20:16:08 +0200

    added gread-menu-dialog.ui

--- src/gui/gread-menu-dialog.c
+++ src/gui/gread-menu-dialog.c
@@ -1,5 +1,6 @@
 #include <gtk/gtk.h>
 #include "gread-menu-dialog.h"
+#include "gread-window.h"
 
 struct _GreadMenuDialog {
   GtkDialog parent;
@@ -9,6 +10,6 @@ G_DEFINE_TYPE (GreadMenuDialog, gread_menu_dialog, GTK_TYPE_DIALOG)
 
 
 GreadMenuDialog *
-gread_menu_dialog_new(void){
-
+gread_menu_dialog_new(GreadAppWindow *win){
+  return g_object_new(GREAD_MENU_DIALOG_TYPE, "transient-for", win, "use-header-bar", TRUE, NULL);
 }
