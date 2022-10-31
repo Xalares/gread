e225bdd6c9523c231ea7e7ed90e6f44945892974
xalares <alexis_de-bacco@hotmail.fr>
2022-10-31 21:23:35 +0100

    Updated app translation

--- src/main.c
+++ src/main.c
@@ -6,11 +6,6 @@
 
 gint main (gint argc, gchar **argv){
 
-  setlocale (LC_ALL, "");
-  bindtextdomain (GETTEXT_PACKAGE, DATADIR "/locale");
-  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-  textdomain (GETTEXT_PACKAGE);
-
   GreadApplication * app = gread_application_new ();
 
   return g_application_run(G_APPLICATION(app), argc, argv);
